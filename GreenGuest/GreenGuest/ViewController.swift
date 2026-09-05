64e6070dacc0f50ccdc967477c103d067f166dc0
VidadiMurad <vidad0murad@gmail.com>
2026-09-05 17:11:20 +0400

    First Commit

---
+++ GreenGuest/GreenGuest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GreenGuest
+//
+//  Created by Vidadi Muradzada on 05.09.26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
