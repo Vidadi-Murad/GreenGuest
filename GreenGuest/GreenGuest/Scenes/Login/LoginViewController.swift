7bb63c553e894b23800e39a3868cb4faa2e2b591
VidadiMurad <vidad0murad@gmail.com>
2026-09-05 19:27:32 +0400

    Created 3 functions for UI and some Variables

--- GreenGuest/GreenGuest/Scenes/Login/LoginViewController.swift
+++ GreenGuest/GreenGuest/Scenes/Login/LoginViewController.swift
@@ -8,8 +8,36 @@
 import UIKit
 import SnapKit
 
+
+
 class LoginViewController: UIViewController {
+    
+    private let azLan = UIButton()
+    private let betweenStick = UILabel()
+    private let enLan = UIButton()
+    private let lanStack = UIStackView()
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .white
+        setupUI()
+        setupLayout()
+        setupAction()
+    }
+    
+    
+    private func setupUI() {
+        
+    }
+    
+    private func setupLayout() {
+        
+    }
+    
+    private func setupAction() {
+        
     }
 }
