615842be5f28c9e7b6d6169c6c9c1eff4c22e30b
VidadiMurad <vidad0murad@gmail.com>
2026-09-05 19:41:34 +0400

    Added Languages Buttons

--- GreenGuest/GreenGuest/Scenes/Login/LoginViewController.swift
+++ GreenGuest/GreenGuest/Scenes/Login/LoginViewController.swift
@@ -16,7 +16,8 @@ class LoginViewController: UIViewController {
     private let betweenStick = UILabel()
     private let enLan = UIButton()
     private let lanStack = UIStackView()
-    
+    //----------------------------------
+    private let 
     
     
     
@@ -30,14 +31,50 @@ class LoginViewController: UIViewController {
     
     
     private func setupUI() {
+        azLan.setTitle("AZ", for: .normal)
+        azLan.setTitleColor(.gray, for: .normal)
+        azLan.titleLabel?.font = UIFont(name:"DMSans-Regular", size: 16)
+        
+        betweenStick.text = "|"
+        betweenStick.textColor = .gray
+        betweenStick.font = UIFont(name:"DMSans-Regular",size: 20)
+        
+        
+        enLan.setTitle("EN", for: .normal)
+        enLan.setTitleColor(.black, for: .normal)
+        enLan.titleLabel?.font = UIFont(name:"DMSans-Regular", size: 16)
+        
         
     }
     
     private func setupLayout() {
+        view.addSubview(lanStack)
+        [azLan,betweenStick,enLan].forEach{lanStack.addArrangedSubview($0)}
+        lanStack.axis = .horizontal
+        lanStack.spacing = 1
+        
+        lanStack.snp.makeConstraints{ make in
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(12)
+            make.trailing.equalToSuperview().inset(20)
+        }
+        
         
     }
     
     private func setupAction() {
-        
+        azLan.addTarget(self, action: #selector(didTapAz), for: .touchUpInside)
+        enLan.addTarget(self, action: #selector(didTapEn), for: .touchUpInside)
+    }
+    
+    @objc private func didTapAz() {
+        azLan.setTitleColor(.black, for: .normal)
+        betweenStick.textColor = .gray
+        enLan.setTitleColor(.gray, for: .normal)
+    }
+    
+    @objc private func didTapEn() {
+        enLan.setTitleColor(.black, for: .normal)
+        betweenStick.textColor = .gray
+        azLan.setTitleColor(.gray, for: .normal)
     }
 }
